bcf4e2e579a633c8cc7c5be44c07eead5b56069f
David Baruc Zamorano Nolasco <deyvith.lol@gmail.com>
2021-12-09 20:50:48 -0600

    feat: Creación de navegación

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Cotizacion from 'views/Cotizacion';
+import Home from 'views/Home';
+import Layout from 'views/Layout';
+import Plazos from 'views/Plazos';
+import Productos from 'views/Productos';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>LOOKS GOOD</div>} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/plazos" element={<Plazos />} />
+        <Route path="/cotizacion" element={<Cotizacion />} />
+      </Routes>
+    </Layout>
   );
 }
 
